16056646ddaf70fee06e52fb34b0c6c235555d2d
Mike Weaver <michael@triplewhale.com>
2024-01-02 21:44:55 -0500

    better loading

--- src/components/ClientSummary.tsx
+++ src/components/ClientSummary.tsx
@@ -45,17 +45,24 @@ export const ClientSummary = ({ token, shop }: { token: string; shop: string }) 
   }
 
   return loading ? (
-    <div className="grid w-full flex-wrap items-stretch gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {Array.from({ length: 20 }).map((_, j) => (
-        <>
-          <div key={j} className=" w-full rounded border p-4 shadow-md">
-            <div className="mb-4 h-5 w-full animate-pulse bg-gray-300"></div>
-            <div className="mb-3 h-2 w-full animate-pulse bg-gray-200 leading-relaxed"></div>
-            <div className="mb-3 h-7 w-full animate-pulse bg-gray-200 leading-relaxed"></div>
-            <div className="h-12 w-full animate-pulse bg-gray-200 leading-relaxed"></div>
+    <div className="mt-4 w-full">
+      <div>
+        <h3 className="mb-4 mt-10 flex items-center gap-2 text-xl capitalize">
+          <SourceIcons source={'tripleWhale' as IServiceMap} /> Loading...
+        </h3>
+      </div>
+      <div className="grid w-full flex-wrap items-stretch gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {Array.from({ length: 20 }).map((_, j) => (
+          <div className="mt-4">
+            <div key={j} className=" w-full rounded border p-4 shadow-md">
+              <div className="mb-4 h-5 w-full animate-pulse bg-gray-300"></div>
+              <div className="mb-3 h-2 w-full animate-pulse bg-gray-200 leading-relaxed"></div>
+              <div className="mb-3 h-7 w-full animate-pulse bg-gray-200 leading-relaxed"></div>
+              <div className="h-12 w-full animate-pulse bg-gray-200 leading-relaxed"></div>
+            </div>
           </div>
-        </>
-      ))}
+        ))}
+      </div>
     </div>
   ) : (
     <PolarisVizProvider>
@@ -67,7 +74,7 @@ export const ClientSummary = ({ token, shop }: { token: string; shop: string }) 
 
         return (
           filteredGroup.length > 0 && (
-            <div className="mt-4">
+            <div className="mt-4 w-full">
               <div>
                 <h3 className="mb-4 mt-10 flex items-center gap-2 text-xl capitalize">
                   <SourceIcons source={g as IServiceMap} /> {plainTextService}
