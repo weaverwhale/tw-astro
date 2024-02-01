94a95a893827f4b1f2fce5854bca5577628bd5da
Mike Weaver <michael@triplewhale.com>
2024-02-01 16:53:58 -0500

    rearranging and react client:only directive

--- src/components/ServerSummaryChart.tsx
+++ src/components/ServerSummaryChart.tsx
@@ -3,8 +3,6 @@ import RenderIfVisible from 'react-render-if-visible'
 import { PolarisVizProvider } from '@shopify/polaris-viz'
 import { SparkChart } from '@/components/Charts'
 
-import '@shopify/polaris-viz/build/esm/styles.css'
-
 export const ServerSummaryChart = ({
   item,
   plainTextService,
